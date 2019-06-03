9ab8ba0ba2a2e6515c15b457200ac58abefc26d2
dede-kuntoro <50207351+dede-kuntoro@users.noreply.github.com>
2019-06-03 11:03:45 +0700

    Bash

---
+++ bash.sh
@@ -0,0 +1,35 @@
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+rainbow='\e[1;33m'
+figlet -f slant "" | lolcat
+echo [+]"################################[+]
+JANGAN SAMPAI HOAX DI SOSMED
+MEMPENGARUHI PIKIRAN ANDA
+[+]#################################[+]" | lolcat
+sleep 0
+
+
+
+echo  -e $red "" $green ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo $i"==================="$cy" ======================
+Tutorial :  www.termux.dedekuntoro.com"| lolcat
+echo $i"==================="$cy" ======================"| lolcat
+
+
+date | lolcat
+PS1='\[\033[1;31m\]╭─root@\033[1;33m\]dedekuntoro ⚡
+\[\033[1;32m\]╰─➤'
+
